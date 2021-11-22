24a4e413751a8c888e794daffaa01ca5fc5b8d89
Lory-Stan TANASI <stantanasi@gmail.com>
2021-11-22 16:11:14 +0100

    JsonApiResponseConverter support sealed class
    Throw Exception when JSON:API data type not equal to @JsonApiType annotation

--- retrofit-jsonapi-converter/src/main/java/com/tanasi/jsonapi/converter/JsonApiResponseConverter.kt
+++ retrofit-jsonapi-converter/src/main/java/com/tanasi/jsonapi/converter/JsonApiResponseConverter.kt
@@ -1,18 +1,21 @@
 package com.tanasi.jsonapi.converter
 
 import com.tanasi.jsonapi.*
+import com.tanasi.jsonapi.utils.jsonApiType
 import okhttp3.ResponseBody
 import org.json.JSONArray
 import org.json.JSONObject
 import retrofit2.Converter
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
 
 class JsonApiResponseConverter(
-        private val classReference: Class<*>,
-        private val classReferenceList: Class<*>? = null
-) : Converter<ResponseBody, Any?> {
+    private val classReference: Class<*>,
+    private val classReferenceList: Class<*>? = null,
+) : Converter<ResponseBody, Any> {
 
     override fun convert(value: ResponseBody): Any {
         return if (classReferenceList == null)
@@ -32,16 +35,19 @@ class JsonApiResponseConverter(
         val jsonApi = jsonObject.optJSONObject("jsonapi")
 
         return JsonApiBody(
-                json,
-                fromJson(data, included, classReference),
-                included,
-                meta,
-                links,
-                jsonApi
+            json,
+            fromJson(data, included, classReference),
+            included,
+            meta,
+            links,
+            jsonApi
         )
     }
 
-    private fun getCollection(value: ResponseBody, classReference: KClass<*>): JsonApiBody<List<Any>> {
+    private fun getCollection(
+        value: ResponseBody,
+        classReference: KClass<*>,
+    ): JsonApiBody<List<Any>> {
         val json = value.string()
         val jsonObject = JSONObject(json)
 
@@ -57,12 +63,12 @@ class JsonApiResponseConverter(
         }
 
         return JsonApiBody(
-                json,
-                collection,
-                included,
-                meta,
-                links,
-                jsonApi
+            json,
+            collection,
+            included,
+            meta,
+            links,
+            jsonApi
         )
     }
 
@@ -78,80 +84,103 @@ class JsonApiResponseConverter(
             val errorList: MutableList<JsonApiError> = mutableListOf()
             for (i in 0 until errors.length()) {
                 errorList.add(JsonApiError(
-                        errors.optJSONObject(i).optString("id"),
-                        errors.optJSONObject(i).optJSONObject("links"),
-                        errors.optJSONObject(i).optString("status"),
-                        errors.optJSONObject(i).optString("code"),
-                        errors.optJSONObject(i).optString("title"),
-                        errors.optJSONObject(i).optString("detail"),
-                        errors.optJSONObject(i).optJSONObject("source"),
-                        errors.optJSONObject(i).optJSONObject("meta"),
+                    errors.optJSONObject(i).optString("id"),
+                    errors.optJSONObject(i).optJSONObject("links"),
+                    errors.optJSONObject(i).optString("status"),
+                    errors.optJSONObject(i).optString("code"),
+                    errors.optJSONObject(i).optString("title"),
+                    errors.optJSONObject(i).optString("detail"),
+                    errors.optJSONObject(i).optJSONObject("source"),
+                    errors.optJSONObject(i).optJSONObject("meta"),
                 ))
             }
 
             return JsonApiErrorBody(
-                    json,
-                    errorList,
-                    meta,
-                    JsonApiBody.Links.fromJson(links),
-                    JsonApiBody.JsonApi.fromJson(jsonApi)
+                json,
+                errorList,
+                meta,
+                JsonApiBody.Links.fromJson(links),
+                JsonApiBody.JsonApi.fromJson(jsonApi)
             )
         }
     }
 
 
+    private fun <T : Any> fromJson(data: JSONObject, included: JSONArray?, c: KClass<T>): T {
+        if (data.optString("type") != c.jsonApiType) {
+            throw Exception(
+                "${c.qualifiedName} doesn't have the same JSON:API type than data object : ${
+                    c.jsonApiType
+                } != ${data.optString("type")}"
+            )
+        }
 
-    private fun <T : Any> fromJson(data: JSONObject?, included: JSONArray?, c: KClass<T>): T {
-        val attributes: JSONObject? = data?.optJSONObject("attributes")
-        val relationships: JSONObject? = data?.optJSONObject("relationships")
+        val attributes: JSONObject? = data.optJSONObject("attributes")
+        val relationships: JSONObject? = data.optJSONObject("relationships")
 
         val params = c.primaryConstructor?.parameters
-                ?.filter {
-                    when (val annotation = it.annotations.firstOrNull()) {
-                        is JsonApiId -> data?.has("id") ?: false
-                        is JsonApiAttribute -> attributes?.has(annotation.name) ?: false
-                        is JsonApiRelationship -> {
-                            if (relationships?.has(annotation.name) == true) {
-                                when (val relationshipData = relationships.optJSONObject(annotation.name)?.opt("data")) {
-                                    is JSONObject -> true
-                                    is JSONArray -> relationshipData.optJSONObject(0) != null
-                                    else -> false
-                                }
-                            } else false
-                        }
-                        else -> when {
-                            data?.has(it.name) ?: false -> true
-                            attributes?.has(it.name) ?: false -> true
-                            relationships?.has(it.name) ?: false -> {
-                                when (relationships!!.optJSONObject(it.name)?.opt("data")) {
-                                    is JSONObject -> true
-                                    is JSONArray -> true
-                                    else -> false
-                                }
+            ?.filter {
+                when {
+                    it.hasAnnotation<JsonApiId>() -> data.has("id")
+                    it.hasAnnotation<JsonApiAttribute>() -> {
+                        val annotation = it.findAnnotation<JsonApiAttribute>()!!
+                        attributes?.has(annotation.name) ?: false
+                    }
+                    it.hasAnnotation<JsonApiRelationship>() -> {
+                        val annotation = it.findAnnotation<JsonApiRelationship>()!!
+                        if (relationships?.has(annotation.name) == true) {
+                            when (val relationshipData =
+                                relationships.optJSONObject(annotation.name)?.opt("data")) {
+                                is JSONObject -> true
+                                is JSONArray -> relationshipData.optJSONObject(0) != null
+                                else -> false
                             }
-                            else -> false
+                        } else false
+                    }
+                    else -> when {
+                        data.has(it.name) -> true
+                        attributes?.has(it.name) ?: false -> true
+                        relationships?.has(it.name) ?: false -> {
+                            when (relationships!!.optJSONObject(it.name)?.opt("data")) {
+                                is JSONObject -> true
+                                is JSONArray -> true
+                                else -> false
+                            }
+                        }
+                        else -> false
+                    }
+                }
+            }
+            ?.map {
+                it to when {
+                    it.hasAnnotation<JsonApiId>() -> data.optString("id")
+                    it.hasAnnotation<JsonApiAttribute>() -> {
+                        val annotation = it.findAnnotation<JsonApiAttribute>()!!
+                        getAttribute(annotation.name, attributes!!, it.type)
+                    }
+                    it.hasAnnotation<JsonApiRelationship>() -> {
+                        val annotation = it.findAnnotation<JsonApiRelationship>()!!
+                        getRelation(annotation.name, relationships!!, included, it.type)
+                    }
+                    else -> {
+                        when {
+                            data.has(it.name) -> data.optString(it.name)
+                            attributes?.has(it.name) ?: false -> getAttribute(it.name!!,
+                                attributes!!,
+                                it.type)
+                            relationships?.has(it.name) ?: false -> getRelation(it.name!!,
+                                relationships!!,
+                                included,
+                                it.type)
+                            else -> null
                         }
                     }
                 }
-                ?.map {
-                    it to when (val annotation = it.annotations.firstOrNull()) {
-                        is JsonApiId -> data!!.optString("id")
-                        is JsonApiAttribute -> getAttribute(annotation.name, attributes!!, it.type)
-                        is JsonApiRelationship -> getRelation(annotation.name, relationships!!, included, it.type)
-                        else -> {
-                            when {
-                                data?.has(it.name) ?: false -> data?.optString(it.name)
-                                attributes?.has(it.name) ?: false -> getAttribute(it.name!!, attributes!!, it.type)
-                                relationships?.has(it.name) ?: false -> getRelation(it.name!!, relationships!!, included, it.type)
-                                else -> null
-                            }
-                        }
-                    }
-                }
-                ?.toMap()
-                ?: mapOf()
+            }
+            ?.toMap()
+            ?: mapOf()
 
-        return c.primaryConstructor?.callBy(params)!!
+        return c.primaryConstructor!!.callBy(params)
     }
 
 
@@ -167,17 +196,32 @@ class JsonApiResponseConverter(
         }
     }
 
-    private fun getRelation(name: String, relationships: JSONObject, included: JSONArray?, kType: KType): Any? {
+    private fun getRelation(
+        name: String,
+        relationships: JSONObject,
+        included: JSONArray?,
+        kType: KType,
+    ): Any? {
         return when (val relationshipData = relationships.optJSONObject(name)?.opt("data")) {
             is JSONObject -> {
                 val type = relationshipData.optString("type")
                 val id = relationshipData.optString("id")
 
-                val resourceData = included?.find {
+                val resourceData = included?.firstOrNull {
                     it.optString("type") == type && it.optString("id") == id
                 } ?: return null
 
-                fromJson(resourceData, included, kType.classifier as KClass<*>)
+                val c = kType.classifier as KClass<*>
+                fromJson(
+                    resourceData,
+                    included,
+                    when {
+                        c.isSealed -> c.sealedSubclasses
+                            .firstOrNull { it.jsonApiType == type }
+                            ?: throw Exception("Sealed class ${c.qualifiedName} doesn't contain a class with @JsonApiType equals $type")
+                        else -> c
+                    }
+                )
             }
             is JSONArray -> {
                 included?.let {
@@ -186,11 +230,21 @@ class JsonApiResponseConverter(
                         val type = relationshipData.optJSONObject(i).optString("type")
                         val id = relationshipData.optJSONObject(i).optString("id")
 
-                        val resourceData = included.find {
+                        val resourceData = included.firstOrNull {
                             it.optString("type") == type && it.optString("id") == id
                         } ?: continue
 
-                        list.add(fromJson(resourceData, included, kType.arguments.first().type?.classifier as KClass<*>))
+                        val c = kType.arguments.first().type?.classifier as KClass<*>
+                        list.add(fromJson(
+                            resourceData,
+                            included,
+                            when {
+                                c.isSealed -> c.sealedSubclasses
+                                    .firstOrNull { it.jsonApiType == type }
+                                    ?: throw Exception("Sealed class ${c.qualifiedName} doesn't contain a class with @JsonApiType equals $type")
+                                else -> c
+                            }
+                        ))
                     }
                     list
                 } ?: listOf<Any>()
@@ -199,7 +253,8 @@ class JsonApiResponseConverter(
         }
     }
 
-    private fun JSONArray.find(predicate: (JSONObject) -> Boolean): JSONObject? {
+
+    private fun JSONArray.firstOrNull(predicate: (JSONObject) -> Boolean): JSONObject? {
         for (i in 0 until this.length()) {
             if (predicate(this.optJSONObject(i))) {
                 return this.optJSONObject(i)
