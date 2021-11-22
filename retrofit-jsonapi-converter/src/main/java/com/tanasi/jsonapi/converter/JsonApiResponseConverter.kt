c72d8c5a4959e0fd7cf87faff10fbaf8a37f7dbe
Lory-Stan TANASI <stantanasi@gmail.com>
2021-11-22 20:23:49 +0100

    Return MutableList by default inside JsonApiResponseConverter

--- retrofit-jsonapi-converter/src/main/java/com/tanasi/jsonapi/converter/JsonApiResponseConverter.kt
+++ retrofit-jsonapi-converter/src/main/java/com/tanasi/jsonapi/converter/JsonApiResponseConverter.kt
@@ -249,7 +249,7 @@ class JsonApiResponseConverter(
                         ))
                     }
                     list
-                } ?: listOf<Any>()
+                } ?: mutableListOf<Any>()
             }
             else -> null
         }
