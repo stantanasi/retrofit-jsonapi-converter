13b5d72fda9a8ba17aee14e0d31875b6f9ef42e7
Lory-Stan TANASI <stantanasi@gmail.com>
2021-11-22 19:56:19 +0100

    Create extensions methods and properties
    Get JSON:API type
    Get JSON:API id
    Get JSON:API attribute name
    Get JSON:API relationship name

--- retrofit-jsonapi-converter/src/main/java/com/tanasi/jsonapi/utils/Extensions.kt
+++ retrofit-jsonapi-converter/src/main/java/com/tanasi/jsonapi/utils/Extensions.kt
@@ -1,24 +1,46 @@
 package com.tanasi.jsonapi.utils
 
 import com.tanasi.jsonapi.JsonApiAttribute
+import com.tanasi.jsonapi.JsonApiId
 import com.tanasi.jsonapi.JsonApiRelationship
 import com.tanasi.jsonapi.JsonApiType
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
+import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
 
-fun KClass<*>.jsonApiType(): String =
-    this.findAnnotation<JsonApiType>()?.name ?: this.qualifiedName ?: ""
-
-fun KClass<*>.jsonApiAttribute(property: KProperty<*>): String =
-    when (val kParameter = this.primaryConstructor?.parameters?.find { it.name == property.name }) {
-        null -> property.findAnnotation<JsonApiAttribute>()?.name ?: property.name
-        else -> kParameter.findAnnotation<JsonApiAttribute>()?.name ?: property.name
+val KClass<*>.jsonApiType: String
+    get() {
+        return this.findAnnotation<JsonApiType>()?.name
+            ?: throw Exception("${this.qualifiedName} class doesn't have @JsonApiType annotation")
     }
 
-fun KClass<*>.jsonApiRelationship(property: KProperty<*>): String =
-    when (val kParameter = this.primaryConstructor?.parameters?.find { it.name == property.name }) {
-        null -> property.findAnnotation<JsonApiAttribute>()?.name ?: property.name
-        else -> kParameter.findAnnotation<JsonApiAttribute>()?.name ?: property.name
+val Any.jsonApiId: String?
+    get() {
+        val kParameter = this::class.primaryConstructor?.parameters
+            ?.run {
+                firstOrNull { it.hasAnnotation<JsonApiId>() }
+                    ?: firstOrNull { it.name == "id" }
+            }
+            ?: throw Exception("${this::class.qualifiedName} class doesn't have an id property")
+
+        return this::class.declaredMemberProperties
+            .firstOrNull { it.name == kParameter.name }
+            ?.call(this) as String?
     }
+
+fun KProperty<*>.jsonApiAttributeName(c: KClass<*>): String =
+    this::class.primaryConstructor?.parameters
+        ?.firstOrNull { it.name == this.name }
+        ?.findAnnotation<JsonApiAttribute>()
+        ?.name
+        ?: this.name
+
+fun KProperty<*>.jsonApiRelationshipName(c: KClass<*>): String =
+    this::class.primaryConstructor?.parameters
+        ?.firstOrNull { it.name == this.name }
+        ?.findAnnotation<JsonApiRelationship>()
+        ?.name
+        ?: this.name
