8cbf2f26a80092335b35aa68cf0fff52724a6ef4
Lory-Stan TANASI <stantanasi@gmail.com>
2021-11-23 09:55:11 +0100

    Describe JsonApiResponse

--- app/src/main/java/com/tanasi/jsonapi/example/fragments/MainViewModel.kt
+++ app/src/main/java/com/tanasi/jsonapi/example/fragments/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.tanasi.jsonapi.example.fragments
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tanasi.jsonapi.JsonApiParams
@@ -9,6 +10,10 @@ import com.tanasi.jsonapi.example.services.MainService
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
+
+    companion object {
+        const val TAG = "MainViewModel"
+    }
 
     fun getArticles() = viewModelScope.launch {
         val response = MainService.build().getArticles(
@@ -51,6 +56,52 @@ class MainViewModel : ViewModel() {
                 response.body.data!! // Article created
             }
             else -> TODO()
+        }
+    }
+
+    fun test() = viewModelScope.launch {
+        val response = MainService.build().getArticle(
+            id = "1"
+        )
+        when (response) {
+            is JsonApiResponse.Success -> {
+                response.headers // okhttp3.Headers
+                response.code // Int (e.g., 2xx)
+
+                response.body.jsonApi?.version // String (e.g., "1.0")
+                response.body.included // JSONArray
+                response.body.links?.first // String (e.g., "http://example.com/...")
+                response.body.meta // JSONObject
+
+                response.body.raw // String (e.g., " {"data":{"type":"articles", ... ")
+            }
+            is JsonApiResponse.Error.ServerError -> {
+                response.body.errors.forEach {
+                    it.id // String
+                    it.links?.about // String
+                    it.status // String
+                    it.code // String
+                    it.title // String
+                    it.detail // String
+                    it.source?.pointer // String
+                    it.source?.parameter // String
+                    it.meta // String
+                }
+            }
+            is JsonApiResponse.Error.NetworkError -> {
+                Log.e(
+                    TAG,
+                    "Network error: ",
+                    response.error // IOException
+                )
+            }
+            is JsonApiResponse.Error.UnknownError -> {
+                Log.e(
+                    TAG,
+                    "Unknown error: ",
+                    response.error // Throwable
+                )
+            }
         }
     }
 }
